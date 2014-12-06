eb9912b5053273b92c50a030bff58e953d0f85a0
Dražen Odobašić <dodobas@candela-it.com>
2014-12-06 09:13:07 +0100

    Refactor code to utils.py

--- django_project/sunlumo_mapserver/renderer.py
+++ django_project/sunlumo_mapserver/renderer.py
@@ -2,94 +2,22 @@
 import logging
 LOG = logging.getLogger(__name__)
 
-import os
-import contextlib
-
-from PyQt4.QtXml import QDomDocument
 from PyQt4.QtCore import QSize, QBuffer, QIODevice
 from PyQt4.QtGui import QColor, QImage, QPainter
 
 from qgis.core import (
-    QgsRasterLayer,
-    QgsVectorLayer,
-    QgsRectangle,
-    QgsMapLayerRegistry,
     QgsMapRendererCustomPainterJob,
     QgsCoordinateReferenceSystem,
-    QgsMapSettings
+    QgsMapSettings,
+    QgsRectangle
 )
 
-
-@contextlib.contextmanager
-def change_directory(path):
-    """
-    A context manager which changes the working directory to the given
-    path, and then changes it back to its previous value on exit.
-    """
-
-    prev_cwd = os.getcwd()
-    os.chdir(path)
-    try:
-        yield
-    finally:
-        os.chdir(prev_cwd)
+from .utils import SunlumoProject, change_directory
 
 
-# QgsProject.instance().setFileName(QGIS_PROJECT)
-# if not(QgsProject.instance().read()):
-    # sys.exit(1)
-# QgsMapLayerRegistry.instance().mapLayers().values()
-
-
-class SunlumoProject:
-
-    def __init__(self, project_file):
-        self._parseProject(project_file)
-        self.project_root = os.path.dirname(project_file)
-
-    def _readLayers(self):
-        layers = self.doc.elementsByTagName('maplayer')
-        for i in xrange(layers.size()):
-            yield layers.at(i)
-
-    def _readAttrs(self, layer):
-        attrs = layer.attributes()
-        for i in xrange(attrs.size()):
-            yield attrs.item(i)
-
-    def _getAttr(self, layer, attr):
-        attrs = layer.attributes()
-        return attrs.namedItem(attr).toAttr()
-
-    def _parseProject(self, project_file):
-        with open(project_file, 'rb') as project_data:
-            self.doc = QDomDocument()
-            self.doc.setContent(project_data.read())
-
-    def _parseLayers(self):
-        # remove all layers from the map registry
-        QgsMapLayerRegistry.instance().removeAllMapLayers()
-        loaded_layers = []
-        for layer in self._readLayers():
-            layer_type = self._getAttr(layer, 'type').value()
-            if layer_type == 'vector':
-                qgsLayer = QgsVectorLayer()
-            elif layer_type == 'raster':
-                qgsLayer = QgsRasterLayer()
-
-            # read layer from XML
-            qgsLayer.readLayerXML(layer.toElement())
-            # add layer to the QgsMapLayerRegistry
-
-            if qgsLayer.isValid():
-                QgsMapLayerRegistry.instance().addMapLayer(qgsLayer)
-                # add layer to the internal layer registry
-                loaded_layers.append(qgsLayer.id())
-                LOG.debug('Loaded layer: %s', qgsLayer.id())
-        return loaded_layers
+class Renderer(SunlumoProject):
 
     def render(self, params):
-
         with change_directory(self.project_root):
 
             crs = QgsCoordinateReferenceSystem()
@@ -107,7 +35,7 @@ class SunlumoProject:
 
             p = QPainter()
             p.begin(img)
-            p.setRenderHint(QPainter.Antialiasing)
+            # p.setRenderHint(QPainter.Antialiasing)
 
             map_settings = QgsMapSettings()
             map_settings.setBackgroundColor(color)
---
+++ django_project/sunlumo_mapserver/utils.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import logging
+LOG = logging.getLogger(__name__)
+
+import contextlib
+import os
+
+from PyQt4.QtXml import QDomDocument
+
+from qgis.core import (
+    QgsRasterLayer,
+    QgsVectorLayer,
+    QgsMapLayerRegistry
+)
+
+
+@contextlib.contextmanager
+def change_directory(path):
+    """
+    A context manager which changes the working directory to the given
+    path, and then changes it back to its previous value on exit.
+    """
+
+    prev_cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
+
+# QgsProject.instance().setFileName(QGIS_PROJECT)
+# if not(QgsProject.instance().read()):
+    # sys.exit(1)
+# QgsMapLayerRegistry.instance().mapLayers().values()
+
+
+class SunlumoProject:
+
+    def __init__(self, project_file):
+        self.project_file = project_file
+        self._parseProject(project_file)
+        self.project_root = os.path.dirname(project_file)
+
+    def _readLayers(self):
+        layers = self.doc.elementsByTagName('maplayer')
+        for i in xrange(layers.size()):
+            yield layers.at(i)
+
+    def _readAttrs(self, layer):
+        attrs = layer.attributes()
+        for i in xrange(attrs.size()):
+            yield attrs.item(i)
+
+    def _getAttr(self, layer, attr):
+        attrs = layer.attributes()
+        return attrs.namedItem(attr).toAttr()
+
+    def _parseProject(self, project_file):
+        with open(project_file, 'rb') as project_data:
+            self.doc = QDomDocument()
+            self.doc.setContent(project_data.read())
+
+    def _parseLayers(self):
+        # remove all layers from the map registry
+        QgsMapLayerRegistry.instance().removeAllMapLayers()
+        loaded_layers = []
+        for layer in self._readLayers():
+            layer_type = self._getAttr(layer, 'type').value()
+            if layer_type == 'vector':
+                qgsLayer = QgsVectorLayer()
+            elif layer_type == 'raster':
+                qgsLayer = QgsRasterLayer()
+
+            # read layer from XML
+            qgsLayer.readLayerXML(layer.toElement())
+            # add layer to the QgsMapLayerRegistry
+
+            if qgsLayer.isValid():
+                QgsMapLayerRegistry.instance().addMapLayer(qgsLayer)
+                # add layer to the internal layer registry
+                loaded_layers.append(qgsLayer.id())
+                LOG.debug('Loaded layer: %s', qgsLayer.id())
+        return loaded_layers
