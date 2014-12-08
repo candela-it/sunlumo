bc9fb108a8104ef2a6c1640d9c2d9f0cd8bcbad9
Dražen Odobašić <dodobas@candela-it.com>
2014-12-08 12:50:16 +0100

    Add basic test script and settings

---
+++ django_project/core/settings/test_dodobas.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from .test import *
+
+DATABASES = {
+    'default': {
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        # Or path to database file if using sqlite3.
+        'NAME': 'sunlumo_dev',
+        # The following settings are not used with sqlite3:
+        'USER': 'dodobas',
+        'PASSWORD': '',
+        # Empty for localhost through domain sockets or '127.0.0.1' for
+        # localhost through TCP.
+        'HOST': 'db',
+        # Set to empty string for default.
+        'PORT': '',
+    }
+}
