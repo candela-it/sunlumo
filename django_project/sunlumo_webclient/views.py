97cb6919bb8a4cbf0ccf50540742a9e7b74f7b0f
Dražen Odobašić <dodobas@candela-it.com>
2014-12-27 13:28:31 +0100

    Inject SL_Project details to the templates

--- django_project/sunlumo_webclient/views.py
+++ django_project/sunlumo_webclient/views.py
@@ -2,8 +2,21 @@
 import logging
 LOG = logging.getLogger(__name__)
 
+import json
+
 from django.views.generic import TemplateView
+
+from sunlumo_mapserver.project import SunlumoProject
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        project = SunlumoProject('/data/simple.qgs')
+
+        context['SL_Details'] = json.dumps(project.getDetails())
+
+        return context
