657e6a9a8d3e2c8b64f8f0f865be2803291a0e8f
Igor Crnkovic <icrni@candela-it.com>
2015-01-13 16:21:07 +0100

    start of Distance Tool Control

--- django_project/lib_js/lib/sl_Project.js
+++ django_project/lib_js/lib/sl_Project.js
@@ -9,6 +9,7 @@ require('./proj');
 
 var SL_LayerControl = require('./sl_layerControl');
 var SL_GFIControl = require('./sl_getfeatureinfoControl');
+var SL_DistanceToolControl = require('./sl_distanceToolControl');
 
 
 var SL_Project = function (options) {
@@ -73,6 +74,8 @@ SL_Project.prototype = {
         // add qgis_GFIControl Layer to the map
         var qgis_GFI_layer = new SL_GFIControl(this.map, this.options);
         this.map.addLayer(qgis_GFI_layer.SL_GFI_Layer);
+
+        new SL_DistanceToolControl(this.map, this.options);
 
         // propagate map events
         this.map.on('singleclick', function(evt) {
