f485f98d2ed2016a103f09bddbae6ee9da98c066
Mauricio Vidulin <mauricio.vidulin@gmail.com>
2015-01-19 14:23:16 +0100

    add development settings for mavidulin

---
+++ django_project/core/settings/dev_mavidulin.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from .dev import *  # NOQA
+
+DATABASES = {
+    'default': {
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        # Or path to database file if using sqlite3.
+        'NAME': 'sunlumo_dev',
+        # The following settings are not used with sqlite3:
+        'USER': 'cit',
+        'PASSWORD': '',
+        # Empty for localhost through domain sockets or '127.0.0.1' for
+        # localhost through TCP.
+        'HOST': '',
+        # Set to empty string for default.
+        'PORT': '',
+    }
+}
+
+import os
+QGIS_PROJECT = os.path.abspath('/home/cit/data/simple.qgs')
+# QGIS project definition
+# QGIS_PROJECT = 'data/simple.qgs'
