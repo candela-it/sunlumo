da5290358cad6f5a9cf64ac2b903ce3ea75daab6
Dražen Odobašić <dodobas@candela-it.com>
2015-01-23 22:29:37 +0100

    Parse and prepare print Layouts

--- django_project/sunlumo_mapserver/printer.py
+++ django_project/sunlumo_mapserver/printer.py
@@ -6,7 +6,8 @@ from qgis.core import (
     QgsComposition,
     QgsPalLabeling,
     QgsMapRenderer,
-    QgsRectangle
+    QgsRectangle,
+    QgsCoordinateReferenceSystem
 )
 
 from .utils import change_directory
@@ -16,7 +17,9 @@ from .project import SunlumoProject
 class Printer(SunlumoProject):
 
     def check_required_params(self, params):
-        req_params = ['tmpFile', 'layout', 'bbox', 'layers', 'transparencies']
+        req_params = [
+            'tmpFile', 'layout', 'bbox', 'layers', 'transparencies', 'srs'
+        ]
         if not(all(param in params.keys() for param in req_params)):
             raise RuntimeError('Missing printer process params!')
 
@@ -25,29 +28,45 @@ class Printer(SunlumoProject):
 
         with change_directory(self.project_root):
 
+            crs = QgsCoordinateReferenceSystem()
+            crs.createFromSrid(params.get('srs'))
+
+            mapRenderer = QgsMapRenderer()
+            mapUnits = crs.mapUnits()
+            mapRenderer.setMapUnits(mapUnits)
+
+            mapExtent = QgsRectangle(*params.get('bbox'))
+            mapRenderer.setExtent(mapExtent)
+
             le = QgsPalLabeling()
-            rndr = QgsMapRenderer()
-            rndr.setLabelingEngine(le)
+            mapRenderer.setLabelingEngine(le)
 
             layers = self.getLayersForRendering(params.get('layers'))
             self.setTransparencies(layers, params.get('transparencies'))
-            rndr.setLayerSet(layers)
+            mapRenderer.setLayerSet(layers)
 
             composer = (
                 self.getLayoutbyName(params['layout'])
                 .firstChildElement('Composition')
             )
 
-            comp = QgsComposition(rndr)
+            comp = QgsComposition(mapRenderer)
             comp.setPlotStyle(QgsComposition.Print)
 
             comp.readXML(composer, self.doc)
             # read composition elements
             comp.addItemsFromXML(composer, self.doc)
 
+            comp.setPrintResolution(90)
+
             # set bbox for the first Map in the layout
             comp_map = comp.getComposerMapById(0)
+
             comp_map.setNewExtent(QgsRectangle(*params.get('bbox')))
+            # comp_map.setNewScale(10000)
+
+            # comp_map.setLayerSet(layers)
+            # comp_map.setKeepLayerSet(True)
 
             # save the file
             comp.exportAsPDF(params['tmpFile'] + '.pdf')
