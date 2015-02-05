ae0048923118bd5ba508e0be74e2a8b7fcc786d7
Dražen Odobašić <dodobas@candela-it.com>
2015-02-05 17:24:50 +0100

    Simple accordion implementation, it just works

--- django_project/lib_js/lib/sl_Project.js
+++ django_project/lib_js/lib/sl_Project.js
@@ -12,6 +12,7 @@ var UI_GetFeatureInfo = require('./ui/getFeatureInfo');
 var UI_PrintControl = require('./ui/printControl');
 var UI_DistanceTool = require('./ui/distanceTool');
 var UI_SpinnerComponent = require('./ui/spinnerComponent');
+var UI_Accordion = require('./ui/accordion');
 
 var SL_Map = require('./sl_map');
 var SL_QGISLayerControl = require('./sl_QGISLayerControl');
@@ -69,15 +70,31 @@ SL_Project.prototype = {
     _initUI: function() {
         // layer control
         var ui_lc = new UI_LayerControl(this.options);
-        m.module(document.getElementById('panelLayers'), {
-            controller: function () {return ui_lc.controller;},
-            view: function (ctrl) {return [ui_lc.view(ctrl)];},
+        var ui_ss = new UI_SimilaritySearch(this.options);
+        var ui_pc = new UI_PrintControl(this.options, {
+            'layers': ui_lc.controller.vm.getLayersParam(),
+            'transparencies': ui_lc.controller.vm.getTransparencyParam()
         });
 
-        var ui_ss = new UI_SimilaritySearch(this.options);
-        m.module(document.getElementById('panelSearch'), {
-            controller: function () {return ui_ss.controller;},
-            view: function (ctrl) {return [ui_ss.view(ctrl)];},
+        var ui_acc = new UI_Accordion(this.options, [{
+                'title': 'Slojevi',
+                'component': ui_lc,
+                'open': true
+            },
+            {
+                'title': 'Pretraživanje',
+                'component': ui_ss
+            },
+            {
+                'title': 'Print',
+                'component': ui_pc
+            }
+        ]);
+
+        m.module(document.getElementById('sidebar'), {
+            controller: function () {return ui_acc.controller;},
+            view: function (ctrl) {return [ui_acc.view(ctrl)];},
+
         });
 
         var ui_gfi = new UI_GetFeatureInfo(this.options);
@@ -86,22 +103,11 @@ SL_Project.prototype = {
             view: function (ctrl) {return [ui_gfi.view(ctrl)];},
         });
 
-        // printControl depends on active layers and transparencies so we need
-        // to pass them as initialState
-        var ui_pc = new UI_PrintControl(this.options, {
-            'layers': ui_lc.controller.vm.getLayersParam(),
-            'transparencies': ui_lc.controller.vm.getTransparencyParam()
-        });
-        m.module(document.getElementById('panelPrint'), {
-            controller: function () {return ui_pc.controller;},
-            view: function (ctrl) {return [ui_pc.view(ctrl)];},
-        });
-
-        var ui_dt = new UI_DistanceTool(this.options);
-        m.module(document.getElementById('distanceToolControl'), {
-            controller: function () {return ui_dt.controller;},
-            view: function (ctrl) {return [ui_dt.view(ctrl)];},
-        });
+        // var ui_dt = new UI_DistanceTool(this.options);
+        // m.module(document.getElementById('distanceToolControl'), {
+        //     controller: function () {return ui_dt.controller;},
+        //     view: function (ctrl) {return [ui_dt.view(ctrl)];},
+        // });
 
         var ui_spin = new UI_SpinnerComponent(this.options);
         m.module(document.getElementById('logo'), {
---
+++ django_project/lib_js/lib/ui/views/accordion.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var m = require('mithril');
+var _ = require('lodash');
+
+
+var VIEW = function (ctrl) {
+    // do not add anything else to the VIEW, it should never initialize anything
+    return render(ctrl);
+};
+
+var render = function(ctrl) {
+    return m('ul.accordion', [
+        ctrl.vm.components.map(function(component, index) {
+            return m('li.accordion-navigation', [
+                m('a', {
+                    'onclick': ctrl.vm.ev_toggleOpen.bind(ctrl, component)
+                }, [component.title()]),
+                m('div', {'class': component.open() ? '' : 'hide'}, [
+                    // render component view
+                    component.view(component.controller)
+                ])
+            ]);
+        })
+    ]
+    );
+};
+
+
+module.exports = VIEW;
