b9215089fc1744280065d938610d2fdb3372070b
Dražen Odobašić <dodobas@candela-it.com>
2015-02-05 20:59:43 +0100

    Remove foundation and other vendor JS

--- django_project/Gruntfile.js
+++ django_project/Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         },
         cssmin: {
             contrib: {
-                src: ['lib_css/normalize.css', 'lib_css/foundation.css', 'lib_css/ol.css', 'lib_css/foundation-icons.css'],
+                src: ['lib_css/ol.css'],
                 dest: 'core/base_static/css/contrib.module.css'
             },
             project: {
--- django_project/lib_js/lib/app.js
+++ django_project/lib_js/lib/app.js
@@ -1,7 +1,3 @@
 'use strict';
 
-require('jquery');
-require('modernizr');
-require('foundation');
-
 window.SL_Project = require('./sl_Project');
