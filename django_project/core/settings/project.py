6c06ce861a62b1a4ba1dae402fd72bab8d469969
Dražen Odobašić <dodobas@candela-it.com>
2015-03-04 13:50:08 +0100

    Add similarity index database structure

--- django_project/core/settings/project.py
+++ django_project/core/settings/project.py
@@ -15,31 +15,3 @@ QGIS_PROJECT_ID = 1
 
 # GetFeatureInfo pixel buffer, how many pixels are used for a search
 QGIS_GFI_BUFFER = 3
-
-QGIS_SIMILARITY_SEARCH = {
-    'kucni broj': {
-        'layer_id': 'KUCNI_BROJEVI20150122212721727',
-        'fields': ['ULICA', 'KUCNI_BROJ'],
-        'pk': 'fid'
-    },
-    'ko lovrec': {
-        'layer_id': 'KO_LOVREC_CESTICE20150122212721706',
-        'fields': ['BROJ_KC'],
-        'pk': 'fid'
-    },
-    'ko medovdolac': {
-        'layer_id': 'KO_MEDOVDOLAC_CESTICE20150122212721718',
-        'fields': ['BROJ_KC'],
-        'pk': 'fid'
-    },
-    'ko opanci': {
-        'layer_id': 'KO_OPANCI_CESTICE20150122212721709',
-        'fields': ['BROJ_KC'],
-        'pk': 'fid'
-    },
-    'ko studenci': {
-        'layer_id': 'KO_STUDENCI_CESTICE20150122212721733',
-        'fields': ['BROJ_KC'],
-        'pk': 'fid'
-    }
-}
--- django_project/sunlumo_similaritysearch/models.py
+++ django_project/sunlumo_similaritysearch/models.py
@@ -1,16 +1,43 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import logging
 
+
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
 LOG = logging.getLogger(__name__)
 
 
-class SimilarityIndex(models.Model):
+@python_2_unicode_compatible
+class IndexSpecification(models.Model):
     project = models.ForeignKey('sunlumo_project.Project')
-    index_name = models.TextField()
+    name = models.CharField(max_length=20)
+    layer = models.ForeignKey('sunlumo_project.Layer')
+    fields = models.ManyToManyField(
+        'sunlumo_project.Attribute', through='IndexAttribute',
+        through_fields=('index', 'attribute')
+    )
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+@python_2_unicode_compatible
+class IndexAttribute(models.Model):
+    index = models.ForeignKey('IndexSpecification')
+    attribute = models.ForeignKey('sunlumo_project.Attribute')
+    primary_key = models.BooleanField(default=False)
+    ordering = models.IntegerField(default=-1)
+
+    def __str__(self):
+        return '{} ({})'.format(self.index, self.attribute)
+
+
+class IndexData(models.Model):
+    index = models.ForeignKey('IndexSpecification')
     feature_id = models.TextField()
     text = models.TextField()
 
     class Meta:
-        index_together = [['project', 'index_name', 'feature_id']]
+        index_together = [['index', 'feature_id']]
