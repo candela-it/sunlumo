5cb24768b7397838eea7fa5d6027c1bbcb76e3a3
Igor Crnkovic <icrni@candela-it.com>
2015-05-19 13:22:03 +0200

    fix for chrome sidebar visibility

---
+++ django_project/core/settings/dev_icrni.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from .dev import *  # NOQA
+
+DATABASES = {
+    'default': {
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        # Or path to database file if using sqlite3.
+        'NAME': 'cit_dev',
+        # The following settings are not used with sqlite3:
+        'USER': 'cit',
+        'PASSWORD': '',
+        # Empty for localhost through domain sockets or '127.0.0.1' for
+        # localhost through TCP.
+        'HOST': '',
+        # Set to empty string for default.
+        'PORT': '',
+    }
+}
